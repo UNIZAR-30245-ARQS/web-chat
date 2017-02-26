fb065f3eecf232aa670b05b459cac4a889a0be0d
rbejar <rbejar@unizar.es>
2017-02-26 11:30:46 +0100

    Cleaning after the latest change

--- socketio_server.js
+++ socketio_server.js
@@ -1,19 +1,10 @@
 var amqpbridge = require('./amqpbridge.js');
 var consts = require('./consts.js');
 
-// To keep track of the clients
-var clientSockets = new Array();
-
 function startSocketIOChannel(http) {
 	var io = require('socket.io')(http);
-	io.on('connection', function(socket){
-		clientSockets[socket.id] = socket;
-		console.log('user ' + socket.id + ' connected');    
-		socket.on('disconnect', function(){
-			// You should remove socket.id from clientSockets here if you want to
-			// use this example to build something (probably a bad idea :-P)
-			console.log('user ' + socket.id + ' disconnected');
-		});
+	io.on('connection', function(socket){		
+		console.log('user ' + socket.id + ' connected');    		
 		socket.on('chat message', function(msg){
 			console.log('message: ' + msg);
 			// Send message to every connected client
