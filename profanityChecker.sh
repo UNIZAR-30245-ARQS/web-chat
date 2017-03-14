0afa5ef50995b9a4e5cbd270c7618c3aca4da50e
rbejar <rbejar@unizar.es>
2017-03-14 17:14:00 +0100

    Added a couple of simple bash launchers to make my life easier

---
+++ profanityChecker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PRIVATE_ENV_VARS_FILE=$HOME/private-env-vars.sh
+# Load environment variables with values that should not be in GitHub
+if [ -f "$PRIVATE_ENV_VARS_FILE" ];
+then   
+   source "$PRIVATE_ENV_VARS_FILE"
+fi
+./gradlew run
